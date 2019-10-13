f9344736b9033e379f607da67bc418143660bb1e
metalnodeug <metalnodeug@gmail.com>
2019-10-13 10:26:54 +0200

    Adding more stylish effect for the flag

--- GuessTheFlagSwiftUI/ContentView.swift
+++ GuessTheFlagSwiftUI/ContentView.swift
@@ -17,7 +17,8 @@ struct ContentView: View {
 
     var body: some View {
         ZStack{
-            Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
 
             VStack(spacing: 30) {
                 VStack {
@@ -35,6 +36,9 @@ struct ContentView: View {
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
                     }
                 }
                 Spacer()
